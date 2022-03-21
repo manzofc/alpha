b3bec188969cd9115b0dc06bc6ec05e9d57b8b0c
manzofc <manzok602@gmail.com>
2022-03-22 04:22:14 +0700

    alpha

---
+++ shop/allpayment.js
@@ -0,0 +1,35 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY ZEEONE OFC
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ 8881 0887 4350 47326
+2. BCA 
+	ㅁ 3901 0887 4350 47326
+3. BNI 
+	ㅁ 881 0887 4350 47326
+4. PERMATA
+	ㅁ 852 8887 4350 47326
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ (Minta Owner)
+2. OVO
+	ㅁ (Scan PP Bot)
+3. DANA
+	ㅁ (Scan PP Bot)
+	
+Semuanya atas nama F ** N**
+
+*📲 PULSA*
+
+Tidak tersedia
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
---
+++ shop/sewa.js
@@ -0,0 +1,28 @@
+const pc_sewa = () => {
+return `「 *PRICE LIST* 」
+							
+⬣ PRICE SEWABOT
+
+⬡ 1 MINGGU = Rp 2.000
+⬡ 2 MINGGU = Rp 3.000
+⬡ 3 MINGGU = Rp 4.000
+⬡ 1 BULAN = Rp 5.000
+⬡ PERMANEN = Rp 70.000
+⬡ PERMANEN + USER PREM = Rp 15.000
+
+
+⬣ PRICE JADIBOT
+
+⬡ JADI BOT PERMANEN 25K
+
+
+⬣ MINAT? PM
+⬡ wa.me/6283830930643
+
+
+NOTE : 
+Bot On 24 Jam Karna Di Run Menggunakan Heroku
+Reset Data Setiap Jam 02:00 `
+	}
+
+exports.pc_sewa = pc_sewa
